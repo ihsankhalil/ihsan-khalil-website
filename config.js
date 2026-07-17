0a5dd3811cd372485c073a60bd0eac7648c02236
ihsankhalil <ik@ihsan-khalil.de>
2026-07-17 08:28:45 +0200

    Internes ZENON CMS eingerichtet

---
+++ config.js
@@ -0,0 +1,6 @@
+window.ZENON_CONFIG = {
+  supabaseUrl: "https://yqkzamvjvcsvtbjzxevz.supabase.co",
+  supabaseAnonKey: "sb_publishable_PpIl5PUJqOkpDJQF-8ubfg_A3tenkxX",
+  mediaBucket: "media",
+  publishFunction: "publish"
+};
