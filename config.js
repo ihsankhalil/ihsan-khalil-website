e362d3b2f4fdbc391931c8a4a086eef82c18a093
Website-Rettung <actions@users.noreply.github.com>
2026-07-17 08:53:11 +0000

    Revert "Internes ZENON CMS eingerichtet"
    This reverts commit 0a5dd3811cd372485c073a60bd0eac7648c02236.

--- config.js
+++
@@ -1,6 +0,0 @@
-window.ZENON_CONFIG = {
-  supabaseUrl: "https://yqkzamvjvcsvtbjzxevz.supabase.co",
-  supabaseAnonKey: "sb_publishable_PpIl5PUJqOkpDJQF-8ubfg_A3tenkxX",
-  mediaBucket: "media",
-  publishFunction: "publish"
-};
